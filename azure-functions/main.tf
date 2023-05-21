4809ea965c9d337099568cfce3299985cd559134
majki2k <76625818+majki2k@users.noreply.github.com>
2023-05-21 13:55:14 +0200

    Update main.tf

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -32,25 +32,37 @@ resource "azurerm_linux_function_app" "test_linux_function_app" {
   name                = var.function_app_name
   location            = azurerm_resource_group.test_resource_group.location
   resource_group_name = azurerm_resource_group.test_resource_group.name
-  app_service_plan_id = azurerm_app_service_plan.test_app_service_plan.id
+  service_plan_id = azurerm_app_service_plan.test_app_service_plan.id
   storage_account_name = azurerm_storage_account.test_storage_account.name
+  site_config {
+    application_stack {
+      python_version = "3.9"
+    }
+  }
 }
 
 resource "azurerm_function_app_function" "test_function_app_function" {
   name                = var.function_name
-  resource_group_name = azurerm_resource_group.test_resource_group.name
   function_app_id     = azurerm_linux_function_app.test_linux_function_app.id
-  storage_account_name = azurerm_storage_account.test_storage_account.name
+   language        = "Python"
+  test_data = jsonencode({
+    "name" = "Azure"
+  })
+  config_json = jsonencode({
+    "bindings" = [
+      {
+        "authLevel" = "function"
+        "direction" = "in"
+        "methods" = [
+          "get",
+          "post",
+        ]
+        "name" = "req"
+        "type" = "httpTrigger"
+      },
+      {
+        "direction" = "out"
+        "name"      = "$return"
+        "type"      = "http"
 
-  triggers = {
-    timer = {
-      schedule = var.function_schedule
-    }
-  }
-
-  app_settings = {
-    key1 = var.function_app_settings1
-    key2 = var.function_app_settings2
-  }
-}
 
