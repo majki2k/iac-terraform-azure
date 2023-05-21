c7a74dbddbc2734553a0896fbee018e648a033ce
majki2k <76625818+majki2k@users.noreply.github.com>
2023-05-21 11:52:49 +0200

    Update variables.tf

--- azure-blob-storage/variables.tf
+++ azure-blob-storage/variables.tf
@@ -1,1 +1,25 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account"
+  type        = string
+}
+
+variable "storage_container_name" {
+  description = "Name of the storage container"
+  type        = string
+}
+
+variable "storage_blob_name" {
+  description = "Name of the storage blob"
+  type        = string
+}
+
+variable "storage_blob_source" {
+  description = "Local path of the file to upload to the storage blob"
+  type        = string
+}
 
