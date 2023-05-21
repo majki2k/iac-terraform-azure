c04ace68e582366bf146fd2e4a081f94c752c1ed
majki2k <76625818+majki2k@users.noreply.github.com>
2023-05-21 11:35:17 +0200

    Update variables.tf

--- azure-functions/variables.tf
+++ azure-functions/variables.tf
@@ -1,1 +1,40 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Location of the resources"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account"
+  type        = string
+}
+
+variable "app_service_plan_name" {
+  description = "Name of the app service plan"
+  type        = string
+}
+
+variable "function_app_name" {
+  description = "Name of the function app"
+  type        = string
+}
+
+variable "function_name" {
+  description = "Name of the function"
+  type        = string
+}
+
+variable "function_schedule" {
+  description = "Schedule for the function trigger"
+  type        = string
+}
+
+variable "function_app_settings" {
+  description = "Additional settings for the function app"
+  type        = map(string)
+}
 
