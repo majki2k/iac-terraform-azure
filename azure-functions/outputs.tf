c5c8e3f0fd6c6c7a2e2f1390f5f3c8765713f8cf
majki2k <76625818+majki2k@users.noreply.github.com>
2023-05-23 13:22:26 +0200

    Update outputs.tf

--- azure-functions/outputs.tf
+++ azure-functions/outputs.tf
@@ -1,5 +1,4 @@
 output "function_app_function_id" {
-  value = azurerm_linux_function_app.test_linux_function_app.id
-  description = "ID of the Azure Function"
+  value = azurerm_linux_function_app.test.id
 }
 
