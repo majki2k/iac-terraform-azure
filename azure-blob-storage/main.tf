0e9a72d08b9f1385c243e5757029998ef0ba5581
majki2k <76625818+majki2k@users.noreply.github.com>
2023-05-23 13:33:35 +0200

    Update main.tf

--- azure-blob-storage/main.tf
+++ azure-blob-storage/main.tf
@@ -1,31 +1,27 @@
-provider "azurerm" {
-  version = "=3.0.0"
+resource "azurerm_resource_group" "test" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-resource "azurerm_resource_group" "test_resource_group" {
-  name     = "test-resource-group"
-  location = "West Europe"
+resource "azurerm_storage_account" "test" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.test.name
+  location                 = azurerm_resource_group.test.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
 }
 
-resource "azurerm_storage_account" "test_storage_account" {
-  name                     = "teststorageaccount"
-  resource_group_name      = azurerm_resource_group.test_resource_group.name
-  location                 = azurerm_resource_group.test_resource_group.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_storage_container" "test" {
+  name                  = var.storage_container_name
+  storage_account_name  = azurerm_storage_account.test.name
+  container_access_type = var.container_access_type
 }
 
-resource "azurerm_storage_container" "test_storage_container" {
-  name                  = "testcontainer"
-  storage_account_name  = azurerm_storage_account.test_storage_account.name
-  container_access_type = "private"
-}
-
-resource "azurerm_storage_blob" "test_storage_blob" {
-  name                   = "testblob.zip"
-  storage_account_name   = azurerm_storage_account.test_storage_account.name
-  storage_container_name = azurerm_storage_container.test_storage_container.name
-  type                   = "Block"
-  source                 = "some_local_file.zip"
+resource "azurerm_storage_blob" "test" {
+  name                   = var.storage_blob_name
+  storage_account_name   = azurerm_storage_account.test.name
+  storage_container_name = azurerm_storage_container.test.name
+  type                   = var.type
+  source                 = var.storage_blob_source
 }
 
