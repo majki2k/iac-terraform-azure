44d60f9a4ac5a2172404be030cf4054d151b5439
majki2k <76625818+majki2k@users.noreply.github.com>
2023-05-23 13:22:23 +0200

    Update main.tf

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -1,24 +1,20 @@
-provider "azurerm" {
-  version = "=3.0.0"
-}
-
-resource "azurerm_resource_group" "test_resource_group" {
+resource "azurerm_resource_group" "test" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_storage_account" "test_storage_account" {
+resource "azurerm_storage_account" "test" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.test_resource_group.name
-  location                 = azurerm_resource_group.test_resource_group.location
+  resource_group_name      = azurerm_resource_group.test.name
+  location                 = azurerm_resource_group.test.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
 }
 
-resource "azurerm_app_service_plan" "test_app_service_plan" {
+resource "azurerm_app_service_plan" "test" {
   name                = var.app_service_plan_name
-  location            = azurerm_resource_group.test_resource_group.location
-  resource_group_name = azurerm_resource_group.test_resource_group.name
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
   kind                = var.kind
   reserved            = var.reserved
 
@@ -28,41 +24,25 @@ resource "azurerm_app_service_plan" "test_app_service_plan" {
   }
 }
 
-resource "azurerm_linux_function_app" "test_linux_function_app" {
+resource "azurerm_linux_function_app" "test" {
   name                = var.function_app_name
-  location            = azurerm_resource_group.test_resource_group.location
-  resource_group_name = azurerm_resource_group.test_resource_group.name
-  service_plan_id = azurerm_app_service_plan.test_app_service_plan.id
-  storage_account_name = azurerm_storage_account.test_storage_account.name
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  service_plan_id = azurerm_app_service_plan.test.id
+  storage_account_name = azurerm_storage_account.test.name
   site_config {
     application_stack {
-      python_version = "3.9"
+      python_version = var.python_version
     }
   }
 }
 
-resource "azurerm_function_app_function" "test_function_app_function" {
+resource "azurerm_function_app_function" "test" {
   name                = var.function_name
-  function_app_id     = azurerm_linux_function_app.test_linux_function_app.id
-   language        = "Python"
-  test_data = jsonencode({
-    "name" = "Azure"
-  })
-  config_json = jsonencode({
-    "bindings" = [
-      {
-        "authLevel" = "function"
-        "direction" = "in"
-        "methods" = [
-          "get",
-          "post",
-        ]
-        "name" = "req"
-        "type" = "httpTrigger"
-      },
-      {
-        "direction" = "out"
-        "name"      = "$return"
-        "type"      = "http"
+  function_app_id     = azurerm_linux_function_app.test.id
+   language        = var.language
+  test_data = var.test_data
+  config_json = var.config_json
+}
 
 
